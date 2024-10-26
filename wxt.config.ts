a97aaa5c096e36379b60ced58cc2b45b21c42199
kevin.liu <xckevin927@gmail.com>
2024-10-26 13:11:52 +0800

    更改icon

--- wxt.config.ts
+++ wxt.config.ts
@@ -6,6 +6,10 @@ export default defineConfig({
   modules: ['@wxt-dev/module-react'],
   outDir: 'dist',
   manifest: {
+    name: 'Quick Scroll',
+    description: 'A Chrome extension to quickly scroll to the top or bottom of a web page.',
+    version: '1.0.0',
+    author: 'liukai',
     permissions: ['storage'],
   }
 });
